311753d897f0a9cbd15c7709d8830262d770c631
Renato Carneiro <renato.carneirofs@gmail.com>
2021-06-13 11:17:48 -0300

    Commit Login
    Exercício Login

---
+++ Paketa-Academy_atividade3/Mini-Projetos/Login/index.js
@@ -0,0 +1,17 @@
+function validate() {
+    if (user.value == 0 || user.value.length < 6) {
+        document.getElementById('error').innerHTML = 'Minímo de 06 caracteres';
+        return false;       
+    }else if (pass.value == 0 || pass.value.length < 6) {
+        document.getElementById('error').innerHTML = 'Minímo de 06 caracteres';
+        return false;  
+    }else {   
+        let a1 = document.querySelector("#user");
+        let a2 = a1.value;
+        alert(`O usuário é ${a2}`); 
+        let a3 = document.querySelector("#pass");
+        let a4 = a3.value;
+        alert(`A senha é ${a4}`);        
+    }
+}
+
