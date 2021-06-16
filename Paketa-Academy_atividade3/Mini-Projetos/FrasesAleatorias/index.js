012272e4065d663152e04132351bb87920b3cac8
Renato Carneiro <renato.carneirofs@gmail.com>
2021-06-16 15:38:32 -0300

    Novas atividades

---
+++ Paketa-Academy_atividade3/Mini-Projetos/FrasesAleatorias/index.js
@@ -0,0 +1,19 @@
+const dogsEl = document.getElementById('dog');
+const get_dogs = document.getElementById('click_dog');
+
+const descripition = [{
+	text: 'Akita inu (em japonês: 秋田犬) é uma raça de cães de grande porte do tipo spitz originária das regiões montanhosas do norte do Japão. Existem duas variedades distintas de Akita: um tipo japonês, comumente chamado Akita inu (inu significa cão em japonês) ou Akita japonês, e um tipo americano, conhecido como Akita americano.',
+
+},{
+	text: 'Border collie (em tradução livre "collie da fronteira") é uma raça canina do tipo collie desenvolvida na região da fronteira anglo-escocesa na Grã-Bretanha para o trabalho de pastorear gado ovino. Popular em seu país de origem, é considerado a raça de cães mais inteligente do mundo, de acordo com o livro de Stanley Coren, A Inteligência dos Cães (1995).',
+
+},{
+	text: 'O husky siberiano (em russo: Сибирский хаски) é uma raça de cães de trabalho e companhia, do tipo Spitz, oriunda da Sibéria na Rússia. Sua função específica é tracionar trenós. Estes cães, ou seus ancestrais, são conhecidos na Sibéria há milhares de anos, em uma região específica a leste dos Montes Urais, inicialmente utilizados como cães multifuncionais foram especialmente selecionados ao longo de centenas de anos pelos Chukchi, um povo que vive na região, para puxar trenós em condições de frio extremo.',
+
+},{
+	text: 'Malamute-do-alaska (em inglês: Alaskan Malamute) é uma raça de cães oriunda do Alasca, nos Estados Unidos. Considerada antiga, tem sua origem imprecisa, embora saiba-se que foram desenvolvidos por tribos do Alasca para o trabalho. Criado para puxar trenós e auxiliar na caça, foi nomeado em homenagem à tribo que primeiramente desenvolveu estes caninos, os Mahlemuts. Resistentes e fortes, foram peças fundamentais para os colonizadores da região durante o período da corrida do ouro.',
+
+},{
+	text: 'Samoiedo[a] (em russo: самоедская собака, transl. samoiedskaïa sobaka), ou samoieda, é uma raça de cães do tipo spitz oriunda do norte da Rússia, na região da Sibéria. O nome da raça deriva das tribos Samoyed, que usavam cães do tipo spitz de várias cores para pastorear renas mais ao sul, e usavam cães brancos para caça e para tração de trenós mais ao norte.',
+
+}];
